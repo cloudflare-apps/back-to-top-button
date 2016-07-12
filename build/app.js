6ce77af2bfb1bf35ec7aaf75721972bde28d3ae1
Teffen Ellis (Eric) <teffen@nirri.us>
2016-07-11 20:49:05 -0700

    Flesh out custom icon option. Fix option update oddities.

--- build/app.js
+++ build/app.js
@@ -16,13 +16,20 @@
 
   var options = INSTALL_OPTIONS;
   var element = void 0;
-  var text = void 0;
+  var icon = void 0;
+
+  var ICONS = {
+    fancy: "<svg viewBox=\"0 0 256 256\" version=\"1.1\">\n      <path d=\"M83.5244052,130.453237 L129.059785,84.9178576 L174.595164,130.453237 L144.213452,130.453237 L144.213452,186.792818 L113.896389,186.792818 L113.896389,130.453237 L83.5244052,130.453237 Z M64.431707,68.715835 L64.431707,75.0983746 L193.678134,75.0983746 L193.678134,68.715835 L64.431707,68.715835 Z\"></path>\n    </svg>",
+    line: "<svg viewBox=\"0 0 256 256\" version=\"1.1\">\n      <path d=\"M88.4020203,153.455844 L128,113.857864 L167.59798,153.455844 L173.254834,147.79899 L128,102.544156 L125.171573,105.372583 L82.745166,147.79899 L88.4020203,153.455844 Z\"></path>\n    </svg>",
+    pointer: "<svg viewBox=\"0 0 256 256\" version=\"1.1\">\n      <path d=\"M92.9062438,130.532138 C89.0010007,134.437382 82.6693513,134.437382 78.7641081,130.532138 C74.858865,126.626895 74.858865,120.295246 78.7641081,116.390003 L115.887214,79.2668969 L121.190515,73.963596 C125.095758,70.0583529 131.427408,70.0583529 135.332651,73.963596 L140.635951,79.2668969 L177.759058,116.390003 C181.664301,120.295246 181.664301,126.626895 177.759058,130.532138 C173.853814,134.437382 167.522165,134.437382 163.616922,130.532138 L138,104.915217 L138,175 C138,180.522848 133.522848,185 128,185 C122.477152,185 118,180.522848 118,175 L118,105.438382 L92.9062438,130.532138 Z\"></path>\n    </svg>",
+    triangle: "<svg viewBox=\"0 0 256 256\" version=\"1.1\">\n      <path d=\"M185.081032,156.382867 L128.006097,99.3079319 L70.9311613,156.382867 L185.081032,156.382867 Z\"></path>\n    </svg>"
+  };
 
   function getColors() {
     var backgroundColor = void 0;
 
-    if (options.backgroundColor) {
-      backgroundColor = tinycolor(options.backgroundColor);
+    if (options.color.strategy === "custom") {
+      backgroundColor = tinycolor(options.color.custom);
     } else {
       var documentBackgroundColor = getComputedStyle(document.body).backgroundColor;
       var components = tinycolor(documentBackgroundColor).toHsl();
@@ -34,7 +41,7 @@
 
     var iconColor = backgroundColor.clone();
 
-    backgroundColor.setAlpha(0.18);
+    backgroundColor.setAlpha(0.2);
 
     return {
       backgroundColor: backgroundColor.toRgbString(),
@@ -88,14 +95,8 @@
     element.setAttribute("visibility", visibility);
   }
 
-  function setShape() {
-    if (!element) return;
-
-    element.setAttribute("shape", options.shape);
-  }
-
   function setColors() {
-    if (!element || !text) return;
+    if (!element || !icon) return;
 
     var _getColors = getColors();
 
@@ -104,46 +105,70 @@
 
 
     element.style.backgroundColor = backgroundColor;
-    text.style.color = iconColor;
+    icon.style.fill = iconColor;
+  }
+
+  function setIcon() {
+    if (!icon) return;
+
+    icon.innerHTML = ICONS[options.icon];
+  }
+
+  function setShape() {
+    if (!element) return;
+
+    element.setAttribute("shape", options.shape);
   }
 
   function updateElement() {
-    element = Eager.createElement({ selector: "body", method: "append" }, element);
+    element = document.createElement("eager-app");
+    icon = document.createElement("eager-icon");
+
     element.setAttribute("app-id", "back-to-top-button");
     element.addEventListener("click", backToTop);
 
-    text = document.createElement("text");
-    text.textContent = "⇧";
-    element.appendChild(text);
-
     setVisibility();
+    setIcon();
+    element.appendChild(icon);
+
     setShape();
     setColors();
+
+    document.body.appendChild(element);
+  }
+
+  function bootstrap() {
+    updateElement();
+
+    window.addEventListener("blur", function () {
+      if (!element) return;
+
+      if (backToTopping) {
+        cancelAnimationFrame(animation);
+        resetPositions();
+        window.scrollTo(0, 0);
+      }
+    });
+
+    window.addEventListener("scroll", setVisibility);
   }
 
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElement);
+    document.addEventListener("DOMContentLoaded", bootstrap);
   } else {
-    updateElement();
+    bootstrap();
   }
-
-  window.addEventListener("blur", function () {
-    if (!element) return;
-
-    if (backToTopping) {
-      cancelAnimationFrame(animation);
-      resetPositions();
-      window.scrollTo(0, 0);
-    }
-  });
-
-  window.addEventListener("scroll", setVisibility);
 
   window.INSTALL_SCOPE = {
     updateColors: function updateColors(nextOptions) {
       options = nextOptions;
 
       setColors();
+    },
+    updateIcon: function updateIcon(nextOptions) {
+      options = nextOptions;
+
+      setIcon();
     },
     updateShape: function updateShape(nextOptions) {
       options = nextOptions;
